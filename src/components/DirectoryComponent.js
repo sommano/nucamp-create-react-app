8b8cd96918699f24310300011e85f5a3c753df21
sommano@gmail.com <sommano@gmail.com>
2020-03-21 17:26:50 -0500

    week2

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import CampsiteInfo from './CampsiteinfoComponent';
 
 class Directory extends Component {
     constructor(props) {
@@ -13,26 +14,26 @@ class Directory extends Component {
         this.setState({selectedCampsite: campsite});
     }
 
-    renderSelectedCampsite(campsite) {
-        if (campsite) {
-            return (
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        return <div />;
-    }
+    // renderSelectedCampsite(campsite) {
+    //     if (campsite) {
+    //         return (
+    //             <Card>
+    //                 <CardImg top src={campsite.image} alt={campsite.name} />
+    //                 <CardBody>
+    //                     <CardTitle>{campsite.name}</CardTitle>
+    //                     <CardText>{campsite.description}</CardText>
+    //                 </CardBody>
+    //             </Card>
+    //         );
+    //     }
+    //     return <div />;
+    // }
 
     render() {
         const directory = this.props.campsites.map(campsite => {
             return (
-                <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
+                <div key={campsite.id} className = "col-md-5 m-1">
+                    <Card onClick = {() => this.onCampsiteSelect(campsite)}>
                         <CardImg width="100%" src={campsite.image} alt={campsite.name} />
                         <CardImgOverlay>
                             <CardTitle>{campsite.name}</CardTitle>
@@ -41,17 +42,12 @@ class Directory extends Component {
                 </div>
             );
         });
-
         return (
             <div className="container">
                 <div className="row">
                     {directory}
                 </div>
-                <div className="row">
-                    <div className="col-md-5 m-1">
-                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
-                    </div>
-                </div>
+                <CampsiteInfo campsite = {this.state.selectedCampsite} />
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'typeface-lobster';
