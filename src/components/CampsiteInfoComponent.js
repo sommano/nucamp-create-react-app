f8330c96c229ac6d66ca77ebd9ef3c883c813350
sommano@gmail.com <sommano@gmail.com>
2020-04-04 14:23:28 -0500

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -25,8 +25,8 @@ class CommentForm extends React.Component {
     }
 
     handleSubmit(values) {
-        console.log('Current state is: ' + JSON.stringify(values));
-        alert('Current state is: ' + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render () {
@@ -98,7 +98,7 @@ function RenderCampsite({campsite}) {
     )
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -112,7 +112,7 @@ function RenderComments({comments}) {
                     )
                     })
                 }
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>
         );
     }
@@ -135,7 +135,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                         <RenderCampsite campsite={props.campsite} />
-                        <RenderComments comments={props.comments} />
+                        <RenderComments 
+                            comments={props.comments}
+                            addComment={props.addComment}
+                            campsiteId={props.campsite.id}
+                        />
                 </div>
             </div>
         ); 
