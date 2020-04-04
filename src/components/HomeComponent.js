547fae7adb23f14f3cefbbaa2c5d88d1dc417b41
sommano@gmail.com <sommano@gmail.com>
2020-04-04 16:54:46 -0500

    Fetch Handling Errors

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -19,8 +19,8 @@ function RenderCard({item, isLoading, errMess}) {
         <Card>
             <CardImg src={baseUrl + item.image} alt={item.name} />
             <CardBody>
-            <CardTitle>{item.name}</CardTitle>
-            <CardText>{item.description}</CardText>
+                <CardTitle>{item.name}</CardTitle>
+                <CardText>{item.description}</CardText>
             </CardBody>
         </Card>
     );
